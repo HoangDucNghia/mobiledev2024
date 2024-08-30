c503fe7ac407f6bc08854d1055efdae1e17b2868
Hoang Duc Nghia <nghiahd.22bi13329@usth.edu.vn>
2024-08-30 13:40:25 +0700

    Update assignment 1

---
+++ android_studio_project/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,57 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*EdgeToEdge.enable(this);
+         setContentView(R.layout.activity_weather);
+         View main = findViewById(R.id.main);
+         main.setBackgroundColor(0xFFFF0000);
+         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+         Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+         v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+         return insets;
+         });**/
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("OnStart","start");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("OnResume", "resume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("OnPause", "pause");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("OnStop", "stop");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("OnDestroy", "destroy");
+    }
+
+}
